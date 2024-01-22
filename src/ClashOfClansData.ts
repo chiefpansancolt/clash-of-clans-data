0db9e233ebdae82a463b70e1d9ed829fe7b0f186
Christopher Pezza <chris.pezza@hey.com>
2024-01-22 10:40:24 -0500

    add cannon details for all bases

---
+++ src/ClashOfClansData.ts
@@ -0,0 +1,5 @@
+import * as HomeVillage from './HomeVillage'
+import * as BuilderBase from './BuilderBase'
+import * as ClanCapital from './ClanCapital'
+
+export { HomeVillage, BuilderBase, ClanCapital }
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import * as ClashOfClansData from './ClashOfClansData'
+
+export { ClashOfClansData }
