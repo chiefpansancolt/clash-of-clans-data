7722063bba31acdcfaad0f9aa4da8afb9ce4ba6f
Christopher Pezza <chris.pezza@hey.com>
2024-01-21 21:35:11 -0500

    Base repo files

---
+++ config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, '../dist/umd'),
+    filename: 'index.js',
+    library: 'exampleTypescriptPackage',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ts(x*)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'config/tsconfig.umd.json',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  },
+}
