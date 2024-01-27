80739057f2a31a6c106f6f18f3041023cea529b2
Christopher Pezza <chris.pezza@hey.com>
2024-01-27 14:37:49 -0500

    add Town Halls to Clan Capital

--- src/clanCapital/clanCapital.ts
+++ src/clanCapital/clanCapital.ts
@@ -5,5 +5,17 @@ import HiddenMegaTesla from '@ClanCapital/defenses/hiddenMegaTesla'
 import BombTower from '@ClanCapital/defenses/bombTower'
 import InfernoTower from '@ClanCapital/defenses/infernoTower'
 import Wall from '@ClanCapital/wall'
+import CapitalHall from '@ClanCapital/capitalHall'
+import DistrictHall from '@ClanCapital/districtHall'
 
-export { Cannon, AirDefense, SuperWizardTower, HiddenMegaTesla, BombTower, InfernoTower, Wall }
+export {
+	Cannon,
+	AirDefense,
+	SuperWizardTower,
+	HiddenMegaTesla,
+	BombTower,
+	InfernoTower,
+	Wall,
+	CapitalHall,
+	DistrictHall,
+}
