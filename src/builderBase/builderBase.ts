7e8428d7c7aeb606ca9bb2016354dadf6b11ee42
Christopher Pezza <chris.pezza@hey.com>
2024-01-27 14:39:10 -0500

    add Town Hall to Builder Base

--- src/builderBase/builderBase.ts
+++ src/builderBase/builderBase.ts
@@ -4,5 +4,6 @@ import Firecrackers from '@BuilderBase/defenses/firecrackers'
 import HiddenTesla from '@BuilderBase/defenses/hiddenTesla'
 import XBow from '@BuilderBase/defenses/xbow'
 import Wall from '@BuilderBase/wall'
+import BuilderHall from '@BuilderBase/builderHall'
 
-export { Cannon, ArcherTower, Firecrackers, HiddenTesla, XBow, Wall }
+export { Cannon, ArcherTower, Firecrackers, HiddenTesla, XBow, Wall, BuilderHall }
